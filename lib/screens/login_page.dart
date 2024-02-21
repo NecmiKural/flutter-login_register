8983d0ed01ccf861852bfca6e95899485ad44fed
neo <necmibkural@gmail.com>
2024-02-21 14:02:09 +0300

    register page ui done

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -38,94 +38,97 @@ class _SignupScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Wrap(
           children: [
-            const SizedBox(height: 20),
-            const Text('Email address'),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(Icons.person),
-                ),
-                hintText: 'Enter your email',
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text('Password'),
-            TextFormField(
-              controller: passwordController,
-              textInputAction: TextInputAction.done,
-              obscureText: _isObscure,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(Icons.lock),
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    });
-                  },
-                  icon: Icon(
-                    _isObscure ? Icons.visibility : Icons.visibility_off,
-                  ),
-                ),
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(height: 10),
-            // GestureDetector(
-            //   onTap: () {},
-            //   child: const Text(
-            //     'Forgot password?',
-            //     style: TextStyle(
-            //       color: Colors.deepPurple,
-            //     ),
-            //   ),
-            // ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text("Don't have an account? "),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RegisterScreen()),
-                    );
-                  },
-                  child: const Text(
-                    'Sing Up',
-                    style: TextStyle(
-                      color: Colors.deepPurple,
+                const SizedBox(height: 20),
+                const Text('Email address'),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.person),
+                    ),
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text('Password'),
+                TextFormField(
+                  controller: passwordController,
+                  textInputAction: TextInputAction.done,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.lock),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                      icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    ),
+                    hintText: 'Enter your password',
+                  ),
+                ),
+                //const SizedBox(height: 10),
+                // GestureDetector(
+                //   onTap: () {},
+                //   child: const Text(
+                //     'Forgot password?',
+                //     style: TextStyle(
+                //       color: Colors.deepPurple,
+                //     ),
+                //   ),
+                // ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                )
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegisterScreen()),
+                        );
+                      },
+                      child: const Text(
+                        'Sing Up',
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ],
--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -13,12 +13,17 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<RegisterScreen> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController dateController = TextEditingController();
+  final TextEditingController biographyController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   bool _isObscure = true;
 
   @override
   void dispose() {
+    nameController.dispose();
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
@@ -38,99 +43,158 @@ class _SignupScreenState extends State<RegisterScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Wrap(
           children: [
-            const SizedBox(height: 20),
-            const Text('Email address'),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(Icons.person),
-                ),
-                hintText: 'Enter your email',
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text('Password'),
-            TextFormField(
-              controller: passwordController,
-              textInputAction: TextInputAction.done,
-              obscureText: _isObscure,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(Icons.lock),
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    });
-                  },
-                  icon: Icon(
-                    _isObscure ? Icons.visibility : Icons.visibility_off,
-                  ),
-                ),
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(height: 10),
-            // GestureDetector(
-            //   onTap: () {},
-            //   child: const Text(
-            //     'Forgot password?',
-            //     style: TextStyle(
-            //       color: Colors.deepPurple,
-            //     ),
-            //   ),
-            // ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Register',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text("Do you have an account? "),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginScreen()),
-                    );
-                  },
-                  child: const Text(
-                    'Log in',
-                    style: TextStyle(
-                      color: Colors.deepPurple,
+                const SizedBox(height: 20),
+                const Text('Name'),
+                TextFormField(
+                  controller: nameController,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.person),
+                    ),
+                    hintText: 'Enter your name',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text('Birthdate'),
+                TextField(
+                  controller: dateController,
+                  decoration: const InputDecoration(
+                      hintText: 'Select birthdate',
+                      filled: true,
+                      prefixIcon: Icon(Icons.calendar_today),
+                      enabledBorder:
+                          OutlineInputBorder(borderSide: BorderSide.none),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.deepPurple))),
+                  readOnly: true,
+                  onTap: _selectDate,
+                ),
+                const SizedBox(height: 10),
+                const Text('Biography'),
+                TextFormField(
+                  controller: biographyController,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.text_snippet_sharp),
+                    ),
+                    hintText: 'Enter your biography',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text('Email address'),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.mail),
+                    ),
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text('Password'),
+                TextFormField(
+                  controller: passwordController,
+                  textInputAction: TextInputAction.done,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.lock),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                      icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    ),
+                    hintText: 'Enter your password',
+                  ),
+                ),
+                //const SizedBox(height: 10),
+                // GestureDetector(
+                //   onTap: () {},
+                //   child: const Text(
+                //     'Forgot password?',
+                //     style: TextStyle(
+                //       color: Colors.deepPurple,
+                //     ),
+                //   ),
+                // ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                )
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Do you have an account? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      child: const Text(
+                        'Log in',
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime? pickedDate = await showDatePicker(
+        context: context,
+        firstDate: DateTime(1914),
+        lastDate: DateTime(DateTime.now().year - 18));
+
+    if (pickedDate != null) {
+      setState(() {
+        dateController.text = pickedDate.toString().split(" ")[0];
+      });
+    }
   }
 }

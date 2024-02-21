91a8da44bd638860e7b557a076dff5f47527dcfa
neo <necmibkural@gmail.com>
2024-02-21 09:20:59 +0300

    firebase connected

---
+++ lib/model/user.dart
@@ -0,0 +1,17 @@
+class UserModel {
+  final String? id;
+  final String? name;
+  final String? displayName;
+  final String? email;
+  final String? bday;
+  final String? bio;
+  final String? hobbies;
+  UserModel(
+      {this.id,
+      this.name,
+      this.displayName,
+      this.email,
+      this.bday,
+      this.bio,
+      this.hobbies});
+}
